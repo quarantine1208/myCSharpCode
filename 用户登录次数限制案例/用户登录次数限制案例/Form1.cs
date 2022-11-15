20aa7afca98107ec48241dbdadd21efcf26ca94b
quarantine1208 <116897266+quarantine1208@users.noreply.github.com>
2022-11-16 00:34:20 +0800

    #用户登录次数限制案例

--- 用户登录次数限制案例/用户登录次数限制案例/Form1.cs
+++ 用户登录次数限制案例/用户登录次数限制案例/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace 用户登录次数限制案例
 {
@@ -19,7 +21,54 @@ namespace 用户登录次数限制案例
 
         private void btnLogin_Click(object sender, EventArgs e)
         {
+            //1. 根据用户ID
+            string connStr = ConfigurationManager.ConnectionStrings["conn"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    conn.Open();
+                    cmd.CommandText = @"SELECT [UserId]
+                                          ,[UserName]
+                                          ,[UserPwd]
+                                          ,[LastErrorDateTime]
+                                          ,[ErrorTimes]
+                                      FROM [myTestDB].[dbo].[UserInfo] 
+                                      WHERE UserName='" + txtUserName.Text.Trim() + "'and UserPwd='" + txtUserPwd.Text.Trim() + "'";
+                    bool isHasData = false;
+                    UserInfo uInfo = null; //封装查询来的数据
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            uInfo = new UserInfo();
+                            uInfo.UserId = int.Parse(reader["UserId"].ToString());
+                            uInfo.UserPwd = reader["UserPwd"].ToString();
+                            uInfo.LastErrorDate = DateTime.Parse(reader["LastErrorDateTime"].ToString());
+                            uInfo.ErrorTimes = int.Parse(reader["ErrorTimes"].ToString());
 
+                        }
+                        //判断查询结果中是否有数据，有则返回True,没有则返回False
+                        isHasData = reader.HasRows;
+                    }//SqlDataReader reader = cmd.ExecuteReader() 该花括号执行结束之前，连接一直没有关闭，这时reader一直占用Connection对象, 其他Command对象不能使用Connection对象
+                    if (!isHasData) //如果没有数据
+                    {
+                        cmd.CommandText = @"update [myTestDB].[dbo].[UserInfo]  set [ErrorTimes]=ErrorTimes+1,[LastErrorDateTime]=getdate() where [UserName]='" + txtUserName.Text.Trim() + "'";
+                        cmd.ExecuteNonQuery();
+                        return;
+                    }
+                    if (uInfo.ErrorTimes <= 3 || DateTime.Now.Subtract(uInfo.LastErrorDate).Minutes>15)
+                    {
+                        MessageBox.Show("登录成功");
+                        cmd.CommandText = @"update [myTestDB].[dbo].[UserInfo]  set [ErrorTimes]=0 where [UserId]='" + uInfo.UserId+"'";
+                        cmd.ExecuteNonQuery();
+                    }
+                    else
+                    {
+                        MessageBox.Show("用户名密码不正确");
+                    }
+                }
+            }
         }
     }
 }
---
+++ 用户登录次数限制案例/用户登录次数限制案例/UserInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 用户登录次数限制案例
+{
+    public class UserInfo
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string UserPwd { get; set; }
+        public int ErrorTimes { get; set; }
+        public DateTime LastErrorDate { get; set; }
+    }
+}
